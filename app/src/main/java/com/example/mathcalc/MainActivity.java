233dafacfcec4de0b552abe5a547d46687981888
mmoulton-nscc <w0440932@nscc.ca>
2021-10-14 01:56:50 -0300

    mostly functional

--- app/src/main/java/com/example/mathcalc/MainActivity.java
+++ app/src/main/java/com/example/mathcalc/MainActivity.java
@@ -1,44 +1,62 @@
 package com.example.mathcalc;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.os.*;
 import android.view.*;
 import android.widget.*;
 
 public class MainActivity extends AppCompatActivity
 {
-    //views
-    Button btnOne;
-    Button btnTwo;
-    Button btnThree;
-    Button btnFour;
-    Button btnFive;
-    Button btnSix;
-    Button btnSeven;
-    Button btnEight;
-    Button btnNine;
-    Button btnZero;
-
-    Button btnMultiply;
-    Button btnDivide;
-    Button btnPlus;
-    Button btnMinus;
-
-    Button btnEquals;
-    Button btnClear;
-    Button btnFlip;
-    Button btnDecimal;
+    calculationClass calc = new calculationClass();
 
 
 
+    //text views
+    private TextView textCurrentDisplay;
+
+    private Button btnOne;
+    private Button btnTwo;
+    private Button btnThree;
+    private Button btnFour;
+    private Button btnFive;
+    private Button btnSix;
+    private Button btnSeven;
+    private Button btnEight;
+    private Button btnNine;
+    private Button btnZero;
+
+    private Button btnMultiply;
+    private Button btnDivide;
+    private Button btnPlus;
+    private Button btnMinus;
+
+    private Button btnEquals;
+    private Button btnClear;
+    private Button btnBackspace;
+    private Button btnFlip;
+    private Button btnDecimal;
+
+    //keeping track of things
+    private String firstNum = ""; //why I didn't use strings and convert then before the calc, I don't have an answer, but I'm too far invested in the way I'm doing it now - update: it was a pain but I changed things to work with strings lol
+    private String op = "";
+    private String secondNum = "" ;
+    private boolean opStarted = false; //checks if an initial op has been pressed
+    private boolean swap = false; //buttons will track first num and trigger swap when an op is pressed, when swap is active secondNum will begin being tracked
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //setup views
+        //text
+        textCurrentDisplay = findViewById(R.id.textCurrentDisplay);
+
         //nums
+        //button views
         btnOne = findViewById(R.id.btnOne);
         btnOne.setOnClickListener(onButtonClicked);
 
@@ -63,7 +81,7 @@ public class MainActivity extends AppCompatActivity
         btnEight = findViewById(R.id.btnEight);
         btnEight.setOnClickListener(onButtonClicked);
 
-        btnNine = findViewById(R.id.btntNine);
+        btnNine = findViewById(R.id.btnNine);
         btnNine.setOnClickListener(onButtonClicked);
 
         btnZero = findViewById(R.id.btnZero);
@@ -84,14 +102,16 @@ public class MainActivity extends AppCompatActivity
         btnMinus = findViewById(R.id.btnMinus);
         btnMinus.setOnClickListener(onButtonClicked);
 
+
+        //etc
         btnEquals = findViewById(R.id.btnEquals);
         btnEquals.setOnClickListener(onButtonClicked);
 
-
-        //etc
-
         btnClear = findViewById(R.id.btnClear);
         btnClear.setOnClickListener(onButtonClicked);
+
+        btnBackspace = findViewById(R.id.btnBackspace);
+        btnBackspace.setOnClickListener(onButtonClicked);
 
         btnFlip = findViewById(R.id.btnFlip);
         btnFlip.setOnClickListener(onButtonClicked);
@@ -103,25 +123,263 @@ public class MainActivity extends AppCompatActivity
 
     public View.OnClickListener onButtonClicked = new View.OnClickListener()
     {
+        @SuppressLint("NonConstantResourceId")
         @Override
         public void onClick(View v)
         {
+
             switch(v.getId())
             {
+                //numbers
                 case R.id.btnOne:
-                    Toast.makeText(getApplicationContext(), "but1", Toast.LENGTH_LONG).show();
+                    decidePlacement("1");
                     break;
                 case R.id.btnTwo:
-                    Toast.makeText(getApplicationContext(), "but2", Toast.LENGTH_SHORT).show();
+                    decidePlacement("2");
                     break;
                 case R.id.btnThree:
-                    Toast.makeText(getApplicationContext(), "but3", Toast.LENGTH_SHORT).show();
+                    decidePlacement("3");
                     break;
-                default:
-                    Toast.makeText(getApplicationContext(), "other", Toast.LENGTH_SHORT).show();
+                case R.id.btnFour:
+                    decidePlacement("4");
+                    break;
+                case R.id.btnFive:
+                    decidePlacement("5");
+                    break;
+                case R.id.btnSix:
+                    decidePlacement("6");
+                    break;
+                case R.id.btnSeven:
+                    decidePlacement("7");
+                    break;
+                case R.id.btnEight:
+                    decidePlacement("8");
+                    break;
+                case R.id.btnNine:
+                    decidePlacement("9");
+                    break;
+                case R.id.btnZero:
+                    decidePlacement("0");
+                    break;
+
+                    //ops
+                case R.id.btnMultiply:
+                    resolveOperator("*");
+                    break;
+                case R.id.btnDivision:
+                    resolveOperator("/");
+                    break;
+                case R.id.btnPlus:
+                    resolveOperator("+");
+                    break;
+                case R.id.btnMinus:
+                    resolveOperator("-");
+                    break;
+
+
+                    //etc
+                case R.id.btnDecimal:
+                    decidePlacement(".");
+                    break;
+                case R.id.btnEquals:
+                    resolveEquals();
+                    break;
+                case R.id.btnClear:
+                    resolveClear();
+                    break;
+                case R.id.btnBackspace:
+                    resolveBackspace();
+                    break;
+                case R.id.btnFlip:
+                    resolveFlip();
                     break;
             }
 
+            //after specific button stuff is resolved, update displays
+            updateDisplays();
+
+            if (firstNum.equals("NaN"))
+            {
+                resolveClear(); //resets completely BEFORE next input is input if there's a divide by 0 error
+                //but AFTER update so the error properly displays
+            }
         }
     };
+    
+    public void decidePlacement(String n) //on number button, decide whether it will be placed in the first or second number
+    {
+        if (swap && opStarted)
+        {
+            secondNum = appendToString(secondNum, n);
+            if (secondNum.equals("."))
+            {
+                secondNum = "0.";
+            }
+        }
+        else
+        {
+            firstNum = appendToString(firstNum, n);
+            if (firstNum.equals("."))
+            {
+                firstNum = "0.";
+            }
+        }
+    }
+
+    public void resolveOperator(String o)
+    {
+        if (opStarted && !secondNum.equals("")) //if this isn't the first op pressed and secondnum has something
+        {
+            firstNum = calc.calculate(Double.parseDouble(firstNum), op, Double.parseDouble(secondNum)); //resolve equation and store it in firstNum
+            resetStates();
+            op = o; //set op after resetting
+            opStarted = true; //set started after resetting
+        }
+        else if (!firstNum.equals(""))
+        {
+            op = o; //only set if number has been entered
+            opStarted = true;
+            swap = true;
+        }
+    }
+
+    public void resolveEquals()
+    {
+        if (swap && !op.equals("") && !secondNum.equals("")) //if in swap state and op/secondnum isn't empty resolve equation
+        {
+            firstNum = calc.calculate(Double.parseDouble(firstNum), op, Double.parseDouble(secondNum)); //resolve equation and store it in firstNum
+            resetStates();
+        }
+    }
+
+    public void resolveClear()
+    {
+        //reset all trackers
+        firstNum = "";
+        swap = false;
+        resetStates();
+    }
+
+    public void resolveBackspace()
+    {
+        String str;
+        StringBuilder newstr = new StringBuilder();
+        if (swap && opStarted)
+        {
+            str = secondNum;
+            if(!str.equals(""))
+            {
+                for (int i = 0; i < str.length() - 1; i++)
+                {
+                    newstr.append(str.charAt(i));
+                }
+                secondNum = newstr.toString();
+            }
+        }
+        else
+        {
+            str = firstNum;
+            if(!str.equals(""))
+            {
+                for (int i = 0; i < str.length() - 1; i++)
+                {
+                    newstr.append(str.charAt(i));
+                }
+                firstNum = newstr.toString();
+
+            }
+        }
+    }
+
+    public void resolveFlip()
+    {
+        String str;
+        StringBuilder newstr = new StringBuilder();
+        if (swap && opStarted)
+        {
+            str = secondNum;
+            if(!str.equals(""))
+            {
+                newstr.append(str.charAt(0)); //test first char to see if it's a negative
+                if(!newstr.equals("-"))//if its not negative
+                {
+                    newstr.deleteCharAt(0);
+                    newstr.append("-");//turn it negative and start the loop
+                    for (int i = 0; i < str.length(); i++)
+                    {
+                        newstr.append(str.charAt(i));
+                    }
+                    secondNum = newstr.toString();
+                }
+                else//if it is negative
+                {
+                    newstr.deleteCharAt(0);
+                    //delete char0 and start loop at 1
+                    for (int i = 1; i < str.length(); i++)
+                    {
+                        newstr.append(str.charAt(i));
+                    }
+                    secondNum = newstr.toString();
+                }
+
+            }
+        }
+        else
+        {
+            str = firstNum;
+            if(!str.equals(""))
+            {
+                newstr.append(str.charAt(0)); //test first char to see if it's a negative
+                if(!newstr.equals("-"))//if its not negative
+                {
+                    newstr.deleteCharAt(0);
+                    newstr.append("-");//turn it negative and start the loop
+                    for (int i = 0; i < str.length(); i++)
+                    {
+                        newstr.append(str.charAt(i));
+                    }
+                    firstNum = newstr.toString();
+
+                }
+                else//if it is negative
+                {
+                    newstr.deleteCharAt(0);
+                    //delete char0 and start loop at 1
+                    for (int i = 1; i < str.length(); i++)
+                    {
+                        newstr.append(str.charAt(i));
+                    }
+                    firstNum = newstr.toString();
+                }
+            }
+        }
+    }
+
+    public void resetStates()
+    {
+        //resets trackers with the exception of initial number and swap, used when equation is executed
+        op = ""; //reset op
+        opStarted = false;
+        secondNum = ""; //reset secondNum
+    }
+    
+    public String appendToString(String num, String i) //on number button, appends new digit to end of current number
+    {
+        if (num.contains(".") && i.equals(".")) //avoiding double decimals
+        {
+            return num;
+        }
+        else
+        {
+            return num+i;
+        }
+    }
+
+    public void updateDisplays()
+    {
+        String str;
+        str = firstNum + " " +  op + " " + secondNum;
+        textCurrentDisplay.setText(str);
+    }
+
 }
---
+++ app/src/main/java/com/example/mathcalc/calculationClass.java
@@ -0,0 +1,32 @@
+package com.example.mathcalc;
+
+import java.text.DecimalFormat;
+
+public class calculationClass
+{
+    public String calculate (double leftNum, String op, double rightNum)
+    {
+        double calc = 0;
+        DecimalFormat format = new DecimalFormat("0.#"); //https://stackoverflow.com/questions/14204905/java-how-to-remove-trailing-zeros-from-a-double
+        switch (op)
+        {
+            case "*":
+               calc = leftNum * rightNum;
+               break;
+            case "+":
+                calc = leftNum + rightNum;
+                break;
+            case "-":
+                calc = leftNum - rightNum;
+                break;
+            case "/":
+                if (rightNum == 0.0)
+                {
+                    return "NaN";
+                }
+                calc = leftNum / rightNum;
+                break;
+        }
+        return format.format(calc)+"";
+    }
+}
